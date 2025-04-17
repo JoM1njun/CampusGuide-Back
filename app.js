bda257525fbf0e1a26efa70a7b9bb3bba5ca6f37
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-04-18 00:54:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const data = require('./db'); // Render PostgreSQL 연결된 Pool
 
 app.post("/import-db", async (req, res) => {
   try {
-    const sql = fs.readFileSync('C:/Users/brian/dumpv2.sql', 'utf8');
-    await data.query(sql);
+    const sql = fs.readFileSync('dump.sql', 'utf8');
+    console.log("Running SQL:", sql);  // 실행할 SQL 쿼리 확인
+    await data.query(sql);  // 쿼리 실행
     res.json({ status: "success", message: "Data imported" });
   } catch (err) {
-    console.error("Import error:", err);
+    console.error("Import error:", err);  // 에러 로그 확인
     res.status(500).json({ status: "error", message: "Failed to import" });
   }
 });
