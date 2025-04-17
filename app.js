5c8cff00b9650680c9ab1f0789d54f9632c630f7
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-04-18 00:50:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.post("/import-db", async (req, res) => {
 });
 
 app.listen(process.env.DB_port, () => {
-  console.log("Server is running on port" + ${process.env.DB_port});
+  console.log("Server is running on port ${process.env.DB_port}");
 });
