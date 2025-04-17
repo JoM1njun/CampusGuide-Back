c0aefcdda53535ee7710105df5e9dcfb607ade2b
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-04-18 01:34:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // .env 파일 불러오기
 
 const express = require("express"); // express 모듈 불러오기
 const cors = require("cors"); // CORS 모듈 불러오기
-const fs = require("fs");  // 파일 시스템 모듈
+const fs = require('fs');  // 파일 시스템 모듈
 const { Pool } = require('pg');
 const app = express(); // express 앱 생성
 const path = require('path');
@@ -204,10 +204,13 @@ app.post("/import-db", async (req, res) => {
   try {
     const sql = fs.readFileSync('dump.sql', 'utf8');
     console.log("Running SQL:", sql);  // 실행할 SQL 쿼리 확인
+    
     await data.query(sql);  // 쿼리 실행
+    
     res.json({ status: "success", message: "Data imported" });
   } catch (err) {
     console.error("Import error:", err);  // 에러 로그 확인
+    
     res.status(500).json({ status: "error", message: "Failed to import" });
   }
 });
