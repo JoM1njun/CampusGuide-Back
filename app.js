014a492a828d4cbe32c15ab2fc6f8c7a2d6b5aeb
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-04-18 00:49:49 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const data = require('./db'); // Render PostgreSQL 연결된 Pool
+
+app.post("/import-db", async (req, res) => {
+  try {
+    const sql = fs.readFileSync('C:/Users/brian/dumpv2.sql', 'utf8');
+    await data.query(sql);
+    res.json({ status: "success", message: "Data imported" });
+  } catch (err) {
+    console.error("Import error:", err);
+    res.status(500).json({ status: "error", message: "Failed to import" });
+  }
+});
+
+app.listen(process.env.DB_port, () => {
+  console.log("Server is running on port" + ${process.env.DB_port});
+});
